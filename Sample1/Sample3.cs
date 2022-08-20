49b41ded362d6fe4e48ccc33057b703a0a3ab9c5
zakumann <samuel742able@gmail.com>
2022-08-20 23:50:29 +0900

    reupload

---
+++ Sample1/Sample3.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+class Sample3
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "Come here C# world!";
+
+        Label lb = new Label();
+        lb.Width = 150;
+        lb.Text = "Let's Start C#!";
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
